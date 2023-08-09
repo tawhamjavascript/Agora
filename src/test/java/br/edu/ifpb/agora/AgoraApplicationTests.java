3bc42d0c65b6cbbded8320380be4d086a5a3be03
tawhamjavascript <taw.ham.balbino9914@gmail.com>
2023-08-09 12:07:05 -0300

    arquitetura geral

---
+++ src/test/java/br/edu/ifpb/agora/AgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.agora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
