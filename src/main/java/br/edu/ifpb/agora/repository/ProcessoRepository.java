2d8e2e4baf929f4b5e83c4b55f85a0e793b7a0d0
tawhamjavascript <taw.ham.balbino9914@gmail.com>
2023-08-15 09:08:42 -0300

    new querys for the db

--- src/main/java/br/edu/ifpb/agora/repository/ProcessoRepository.java
+++ src/main/java/br/edu/ifpb/agora/repository/ProcessoRepository.java
@@ -1,5 +1,6 @@
 package br.edu.ifpb.agora.repository;
 
+import br.edu.ifpb.agora.model.Aluno;
 import br.edu.ifpb.agora.model.Processo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,5 +10,40 @@ import java.util.List;
 public interface ProcessoRepository extends JpaRepository<Processo, Long> {
     @Query("SELECT pr FROM Professor p JOIN p.colegiados c JOIN c.reunioes r JOIN r.processos pr WHERE p.id = ?")
     public List<Processo> AllProcessosByProfessor(Long idProfessor);
+
+    @Query("SELECT pr.interessado from Processo pr where pr.interessado = ?")
+    public List<Processo> AllProcessByAluno(Aluno aluno);
+
+    @Query("select pr from Processo pr where pr.interessado = ? and pr.assunto.nome = ?")
+    public List<Processo> AllProcessByAlunoPerSubject(Aluno aluno, String subject);
+
+    @Query("select pr from Processo pr where pr.interessado = ? and pr.emPauta = ?")
+    public List<Processo> AllProcessByAlunoPerStatus(Aluno aluno, Boolean pauta);
+
+    @Query("select pr from Processo pr where pr.interessado = ? and pr.assunto.nome = ? order by pr.dataRecepcao ASC")
+    public List<Processo> AllProcessByAlunoPerSubjectOrderByDate(Aluno aluno, String subject);
+
+    @Query("select pr from Processo pr where pr.interessado = ? and pr.emPauta = ? order by pr.dataRecepcao ASC")
+    public List<Processo> AllProcessByAlunoPerStatusOrderByDate(Aluno aluno, Boolean pauta);
+
+    @Query("select pr from Coordenador c join c.colegiados co join co.reunioes r join r.processos pr where c.id = ? and co.id = ?")
+    public List<Processo> AllProcessByCoordenador(Long idCoordenador, Long idColegiado);
+
+    @Query("select pr from Coordenador c join c.colegiados co join co.reunioes r join r.processos pr where c.id = ? and co.id = ? and pr.interessado.nome = ?")
+    public List<Processo> AllProcessByCoordenadorPerAluno(Long idCoordenador, Long idColegiado, String aluno);
+
+    @Query("select pr from Coordenador c join c.colegiados co join co.reunioes r join r.processos pr where c.id = ? and co.id = ? and pr.emPauta = ?")
+    public List<Processo> AllProcessByCoordenadorPerStatus(Long idCoordenador, Long idColegiado, Boolean pauta);
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/main/java/br/edu/ifpb/agora/repository/ReuniaoRepository.java
+++ src/main/java/br/edu/ifpb/agora/repository/ReuniaoRepository.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.agora.repository;
 
 import br.edu.ifpb.agora.model.Reuniao;
+import br.edu.ifpb.agora.model.StatusReuniao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +10,14 @@ import java.util.List;
 public interface ReuniaoRepository extends JpaRepository<Reuniao, Long> {
     @Query("SELECT r FROM Professor p JOIN p.colegiados c JOIN c.reunioes r WHERE p.id = ?")
     public List<Reuniao> AllReunioesByProfessor(Long idProfessor);
+
+    @Query("select r from Professor p join p.colegiados c join c.reunioes r where p.id = ? and c.id = ?")
+    public List<Reuniao> AllReunioesByProfessorAndColegiado(Long idProfessor, Long idColegiado);
+
+    @Query("select r from Professor p join p.colegiados c join c.reunioes r where p.id = ? and c.id = ? and r.status = ?")
+    public List<Reuniao> AllReunioesByProfessorAndColegiadoAndStatus(Long idProfessor, Long idColegiado, StatusReuniao status);
+
+
+
+
 }
